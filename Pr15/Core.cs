b6a0410b87128989578a8f390270a9454591b274
qwepubgbeer <qwepubg@mail.ru>
2026-02-26 14:08:37 +0300

    подключил бд

---
+++ Pr15/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr15
+{
+    internal class Core
+    {
+        public static GMO1CEntities Context = new GMO1CEntities();
+    }
+}

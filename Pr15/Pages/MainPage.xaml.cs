90e07097f39bf913805c6894692417bebda425a8
qwepubgbeer <qwepubg@mail.ru>
2026-02-26 10:30:51 +0300

    сделал главную страницу

--- Pr15/Pages/MainPage.xaml.cs
+++ Pr15/Pages/MainPage.xaml.cs
@@ -13,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Pr15
+namespace Pr15.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainPage : Page
     {
-        public MainWindow()
+        public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
